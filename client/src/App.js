ee74a1306f670ef1809b80e55c3dcf16bad03357
yap8 <nebozhenko.arseny@gmail.com>
2022-04-19 03:43:10 +0300

    added another div to App to fix container color bug

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
-import mergeClasses from './helpers/mergeClasses'
 import Login from "./pages/Login"
 import Register from './pages/Register';
 import Home from './pages/Home';
 
 function App() {
-  const classes = {
-    base: 'h-screen bg-slate-50 flex items-center justify-center container',
-    dark: 'dark:bg-slate-700'
-  }
-
   return (
     <Router>
-      <div className={mergeClasses(classes)}>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/home' element={<Home />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Register />} />
-        </Routes>
+      <div className="bg-slate-50">
+        <div className="h-screen flex items-center justify-center container">
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/home' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+          </Routes>
+        </div>
       </div>
     </Router>
   );
