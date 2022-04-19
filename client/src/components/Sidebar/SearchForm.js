068cdcb622da03258bc51b699a5a880737b1bea0
yap8 <nebozhenko.arseny@gmail.com>
2022-04-19 06:28:19 +0300

    added clear form button to search

--- client/src/components/Sidebar/SearchForm.js
+++ client/src/components/Sidebar/SearchForm.js
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { FaSearch } from 'react-icons/fa'
+import { FaSearch, FaTimes } from 'react-icons/fa'
 
 const SearchForm = () => {
   const [formData, setFormData] = useState('')
@@ -8,19 +8,27 @@ const SearchForm = () => {
     setFormData(e.target.value)
   }
 
+  const clearForm = () => setFormData('')
+
   return (
-    <div className="py-4 flex items-center">
-      <label className="absolute left-7" htmlFor="search">
-        <FaSearch className="text-gray-500" />
+    <div className="py-4 flex items-center relative">
+      <label className="absolute left-3" htmlFor="search">
+        <FaSearch className="text-gray-500 text-lg" />
       </label>
       <input
         id="search"
-        className="w-full py-2 pl-10 pr-4 rounded bg-gray-200 text-xl text-gray-800"
+        className="w-full py-2 px-10 rounded bg-gray-200 text-xl text-gray-800"
         type="text"
         placeholder="Search or start new chat"
         value={formData}
         onChange={handleChange}
       />
+      <button
+        className={`absolute right-3 ${formData.length ? '' : 'hidden'}`}
+        onClick={clearForm}
+      >
+        <FaTimes className="text-gray-500 text-lg" />
+      </button>
     </div>
   )
 }
