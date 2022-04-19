1dd159254973846c0f0b145f2ebc39d3bef27a45
yap8 <nebozhenko.arseny@gmail.com>
2022-04-19 07:43:10 +0300

    added unread messaged and date

--- client/src/components/Sidebar/ChatItem.js
+++ client/src/components/Sidebar/ChatItem.js
@@ -1,7 +1,7 @@
 import { FaUserCircle } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 
-const ChatItem = ({ id, name, lastMessage }) => {
+const ChatItem = ({ id, name, lastMessage, unread }) => {
   return (
     <li className="group">
       <Link
@@ -11,9 +11,15 @@ const ChatItem = ({ id, name, lastMessage }) => {
         <div className="w-12 h-12 mr-4">
           <FaUserCircle className="text-5xl text-gray-400" />
         </div>
-        <div className="w-full truncate">
+        <div className="w-full truncate mr-4">
           <h4 className="font-semibold">{name}</h4>
           <p className="truncate">{lastMessage}</p>
+        </div>
+        <div className="flex flex-col items-end">
+          <div className="text-gray-400">28/03/2022</div>
+          {unread ? (
+            <div className="flex items-center justify-center bg-blue-500 text-white w-7 h-7 rounded-full">{unread}</div>
+          ) : ''}
         </div>
       </Link>
     </li>
--- client/src/components/Sidebar/ChatList.js
+++ client/src/components/Sidebar/ChatList.js
@@ -1,12 +1,12 @@
 import ChatItem from "./ChatItem"
 
 const chats = [
-  { id: '1', name: 'name 1', lastMessage: 'Lorem, ipsum dolor.' },
-  { id: '2', name: 'name 2', lastMessage: 'Lorem, ipsum dolor.' },
-  { id: '3', name: 'name 3', lastMessage: 'Lorem, ipsum dolor.' },
-  { id: '4', name: 'some user 1', lastMessage: 'Lorem, ipsum dolor. Lorem, ipsum dolor. Lorem, ipsum dolor. Lorem, ipsum dolor. ' },
-  { id: '5', name: 'some user 2', lastMessage: 'Lorem, ipsum dolor.' },
-  { id: '6', name: 'some user 3', lastMessage: 'Lorem, ipsum dolor.' }
+  { id: '1', name: 'name 1', lastMessage: 'Lorem, ipsum dolor.', unread: 0 },
+  { id: '2', name: 'name 2', lastMessage: 'Lorem, ipsum dolor.', unread: 2 },
+  { id: '3', name: 'name 3', lastMessage: 'Lorem, ipsum dolor.', unread: 0 },
+  { id: '4', name: 'some user 1', lastMessage: 'Lorem, ipsum dolor. Lorem, ipsum dolor. Lorem, ipsum dolor. Lorem, ipsum dolor. ', unread: 1 },
+  { id: '5', name: 'some user 2', lastMessage: 'Lorem, ipsum dolor.', unread: 15 },
+  { id: '6', name: 'some user 3', lastMessage: 'Lorem, ipsum dolor.', unread: 0 }
 ]
 
 const ChatList = () => {
@@ -19,6 +19,7 @@ const ChatList = () => {
             id={chat.id}
             name={chat.name}
             lastMessage={chat.lastMessage}
+            unread={chat.unread}
           />
         ))}
       </ul>
