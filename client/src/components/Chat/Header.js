a22021ccc05e33b23501cfe109987f19398eaf47
yap8 <nebozhenko.arseny@gmail.com>
2022-04-20 05:39:29 +0300

    made chat header icon not collapsable

--- client/src/components/Chat/Header.js
+++ client/src/components/Chat/Header.js
@@ -7,7 +7,9 @@ const Header = () => {
 
   return (
     <div className="flex justify-between items-center bg-white p-4 shadow">
-      <FaUserCircle className="text-5xl text-gray-400 mr-4" />
+      <div className="w-12 h-12 mr-4">
+        <FaUserCircle className="text-5xl text-gray-400" />
+      </div>
       <h1 className="w-full font-semibold text-2xl">{title}</h1>
       <button className="group">
         <BsThreeDotsVertical className="w-6 h-6 text-gray-400 transition group-hover:text-gray-800" />
