e43a1b53f52752a9ae5c46e45480de0c4c655870
yap8 <nebozhenko.arseny@gmail.com>
2022-04-20 03:26:23 +0300

    added chatsReducer

--- client/src/components/Sidebar/ChatList.js
+++ client/src/components/Sidebar/ChatList.js
@@ -1,15 +1,10 @@
+import { useSelector } from "react-redux"
+
 import ChatItem from "./ChatItem"
 
-const chats = [
-  { id: '1', name: 'name 1', lastMessage: { text: 'Lorem, ipsum dolor.', date: '28/03/2022' }, unread: 0 },
-  { id: '2', name: 'name 2', lastMessage: { text: 'Lorem, ipsum dolor.', date: '28/03/2022' }, unread: 2 },
-  { id: '3', name: 'name 3', lastMessage: { text: 'Lorem, ipsum dolor.', date: '28/03/2022' }, unread: 0 },
-  { id: '4', name: 'some user 1', lastMessage: { text: 'Lorem, ipsum dolor. Lorem, ipsum dolor. Lorem, ipsum dolor. Lorem, ipsum dolor.', date: '28/03/2022' }, unread: 1 },
-  { id: '5', name: 'some user 2', lastMessage: { text: 'Lorem, ipsum dolor.', date: '28/03/2022' }, unread: 15 },
-  { id: '6', name: 'some user 3', lastMessage: { text: 'Lorem, ipsum dolor.', date: '28/03/2022' }, unread: 0 },
-]
-
 const ChatList = () => {
+  const chats = useSelector(state => state.chats)
+
   return (
     <div>
       <ul className="h-screen overflow-auto">
--- client/src/redux/reducers/rootReducer.js
+++ client/src/redux/reducers/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
+import chatsReducer from "./chatsReducer";
 import requestReducer from "./requestReducer";
 import userReducer from "./userReducer";
 
 const rootReducer = combineReducers({
   request: requestReducer,
-  user: userReducer
+  user: userReducer,
+  chats: chatsReducer
 })
 
 export default rootReducer
