e986ee949400a8be8106248479f85c49cf6ef003
yap8 <nebozhenko.arseny@gmail.com>
2022-04-20 06:35:02 +0300

    added dummy settings

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import 'react-toastify/dist/ReactToastify.css'
 import Login from "./pages/Login"
 import Register from './pages/Register';
 import Home from './pages/Home';
+import Settings from './pages/Settings';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
             <Route path='/chats/:id' element={<Home />} />
             <Route path='/login' element={<Login />} />
             <Route path='/register' element={<Register />} />
+            <Route path='/settings' element={<Settings />} />
           </Routes>
         </div>
       </div>
