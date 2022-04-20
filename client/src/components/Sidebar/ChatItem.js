9e386355d3c9ebe0f58741a785ca25d9b47c2464
yap8 <nebozhenko.arseny@gmail.com>
2022-04-20 05:50:26 +0300

    added styles for active chat item in aside

--- client/src/components/Sidebar/ChatItem.js
+++ client/src/components/Sidebar/ChatItem.js
@@ -1,11 +1,21 @@
-import { FaUserCircle } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
+import { FaUserCircle } from 'react-icons/fa'
+import { useSelector } from 'react-redux'
+
+import mergeClasses from '../../helpers/mergeClasses'
 
 const ChatItem = ({ id, name, lastMessage, unread }) => {
+  const chat = useSelector(state => state.chat)
+
+  const linkClasses = {
+    base: 'p-4 flex transition group-hover:bg-gray-200',
+    active: chat.id === id ? 'bg-gray-200' : ''
+  }
+
   return (
     <li className="group">
       <Link
-        className="p-4 flex transition group-hover:bg-gray-200"
+        className={mergeClasses(linkClasses)}
         to={`/chats/${id}`}
       >
         <div className="w-12 h-12 mr-4">
--- client/src/redux/reducers/chatsReducer.js
+++ client/src/redux/reducers/chatsReducer.js
@@ -1,10 +1,10 @@
 const initialState = [
-  { id: '1', name: 'name 1', lastMessage: { text: 'Lorem, ipsum dolor.', date: '28/03/2022' }, unread: 0 },
-  { id: '2', name: 'name 2', lastMessage: { text: 'Lorem, ipsum dolor.', date: '28/03/2022' }, unread: 2 },
-  { id: '3', name: 'name 3', lastMessage: { text: 'Lorem, ipsum dolor.', date: '28/03/2022' }, unread: 0 },
-  { id: '4', name: 'some user 1', lastMessage: { text: 'Lorem, ipsum dolor. Lorem, ipsum dolor. Lorem, ipsum dolor. Lorem, ipsum dolor.', date: '28/03/2022' }, unread: 1 },
-  { id: '5', name: 'some user 2', lastMessage: { text: 'Lorem, ipsum dolor.', date: '28/03/2022' }, unread: 15 },
-  { id: '6', name: 'some user 3', lastMessage: { text: 'Lorem, ipsum dolor.', date: '28/03/2022' }, unread: 0 },
+  { id: '123123', name: 'name 1', lastMessage: { text: 'Lorem, ipsum dolor.', date: '28/03/2022' }, unread: 0 },
+  { id: '321321', name: 'name 2', lastMessage: { text: 'Lorem, ipsum dolor.', date: '28/03/2022' }, unread: 2 },
+  { id: '3213211', name: 'name 3', lastMessage: { text: 'Lorem, ipsum dolor.', date: '28/03/2022' }, unread: 0 },
+  { id: '1231213', name: 'some user 1', lastMessage: { text: 'Lorem, ipsum dolor. Lorem, ipsum dolor. Lorem, ipsum dolor. Lorem, ipsum dolor.', date: '28/03/2022' }, unread: 1 },
+  { id: '511111', name: 'some user 2', lastMessage: { text: 'Lorem, ipsum dolor.', date: '28/03/2022' }, unread: 15 },
+  { id: '633333', name: 'some user 3', lastMessage: { text: 'Lorem, ipsum dolor.', date: '28/03/2022' }, unread: 0 },
 ]
 
 const chatsReducer = (state = initialState, action) => {
