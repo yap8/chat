1e53a5b372c0c4f8ff1ca28980b5e42e68357828
yap8 <nebozhenko.arseny@gmail.com>
2022-04-20 05:31:28 +0300

    made chat header change title depending on selected chat

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -5,22 +5,15 @@ import Header from './Header'
 import MessagesList from './MessagesList'
 import Form from './Form'
 
-const messages = [
-  { id: '322222222', content: 'Hello!', sender: '1' },
-  { id: '321321312', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, rerum.', sender: '2' },
-  { id: '123123', content: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat culpa est deleniti sit corporis, iste beatae autem officiis explicabo sint!', sender: '1' }
-]
-
 const Chat = () => {
-  const { id } = useParams()
-
   const chat = useSelector(state => state.chat)
+  // const { id } = useParams()
 
   return (
     <div className="h-screen w-3/4 flex flex-col justify-between">
       {chat && <>
-        <Header name={'name ' + id} />
-        <MessagesList messages={messages} />
+        <Header />
+        <MessagesList />
         <Form />
       </>}
     </div>
--- client/src/components/Chat/Header.js
+++ client/src/components/Chat/Header.js
@@ -1,11 +1,14 @@
 import { BsThreeDotsVertical } from 'react-icons/bs'
 import { FaUserCircle } from 'react-icons/fa'
+import { useSelector } from 'react-redux'
 
-const Header = ({ name }) => {
+const Header = () => {
+  const { title } = useSelector(state => state.chat)
+
   return (
     <div className="flex justify-between items-center bg-white p-4 shadow">
       <FaUserCircle className="text-5xl text-gray-400 mr-4" />
-      <h1 className="w-full font-semibold text-2xl">{name}</h1>
+      <h1 className="w-full font-semibold text-2xl">{title}</h1>
       <button className="group">
         <BsThreeDotsVertical className="w-6 h-6 text-gray-400 transition group-hover:text-gray-800" />
       </button>
