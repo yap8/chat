20930d8b955cd5c3386533563f77fdc6270b654b
yap8 <nebozhenko.arseny@gmail.com>
2022-04-21 04:39:28 +0300

    added menu to Message

--- client/src/components/Chat/Message.js
+++ client/src/components/Chat/Message.js
@@ -1,20 +1,31 @@
+import { Menu, MenuItem, MenuButton } from '@szhsin/react-menu'
+import '@szhsin/react-menu/dist/transitions/slide.css'
+import '@szhsin/react-menu/dist/index.css'
 import { useSelector } from 'react-redux'
-import mergeClasses from '../../helpers/mergeClasses'
 
 const Message = ({ message }) => {
   const user = useSelector(state => state.user)
 
-  const classes = {
-    base: 'py-2 px-4 rounded bg-white shadow mb-2 flex relative first:mb-0',
-    sender: message.sender_id === user.id ? 'ml-auto' : 'mr-auto'
-  }
-
   const time = message.created_at.split(' ')[1].substring(0, message.created_at.split(' ')[1].length - 3)
 
   return (
-    <li className={mergeClasses(classes)}>
-      <p className="mr-4">{message.content}</p>
-      <p className="text-gray-500 relative top-1">{time}</p>
+    <li className={`relative mb-2 first:mb-0 ${message.sender_id === user.id ? ' ml-auto' : ' mr-auto'}`}>
+      <Menu
+        transition
+        menuButton={(
+          <MenuButton className="py-2 px-4 rounded bg-white shadow flex transition hover:bg-gray-200">
+            <p className="mr-4">{message.content}</p>
+            <p className="text-gray-500 relative top-1">{time}</p>
+          </MenuButton>
+        )}
+      >
+        <MenuItem
+          className="py-2 px-6"
+        >Copy</MenuItem>
+        <MenuItem
+          className="py-2 px-6"
+        >Delete</MenuItem>
+      </Menu>
     </li>
   )
 }
