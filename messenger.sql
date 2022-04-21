998bf4e75a7bfe834dcaa0886c95852d82c1d041
yap8 <nebozhenko.arseny@gmail.com>
2022-04-21 06:30:04 +0300

    updated db dump

--- messenger.sql
+++ messenger.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 21, 2022 at 12:04 AM
+-- Generation Time: Apr 21, 2022 at 05:29 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
@@ -34,6 +34,15 @@ CREATE TABLE `chats` (
   `created_at` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `chats`
+--
+
+INSERT INTO `chats` (`id`, `title`, `creator_id`, `created_at`) VALUES
+(16, 'some name\'s chat', 36, '2022-04-21 01:13:54'),
+(17, 'some name\'s chat', 36, '2022-04-21 05:28:36'),
+(18, 'some name\'s chat', 36, '2022-04-21 06:12:02');
+
 -- --------------------------------------------------------
 
 --
@@ -48,6 +57,21 @@ CREATE TABLE `messages` (
   `created_at` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `messages`
+--
+
+INSERT INTO `messages` (`id`, `chat_id`, `sender_id`, `content`, `created_at`) VALUES
+(12, 16, 37, 'lorem ipsum dolor 2...', '2022-04-21 03:51:22'),
+(13, 16, 36, 'яппи', '2022-04-21 03:53:18'),
+(14, 16, 37, 'ммм знаю его он очень крутой', '2022-04-21 03:53:35'),
+(15, 16, 36, 'даааааа', '2022-04-21 03:53:42'),
+(23, 16, 36, 'asdfasfdsaf', '2022-04-21 05:44:41'),
+(25, 17, 36, '31212123123', '2022-04-21 05:46:24'),
+(26, 17, 36, '123123213123123', '2022-04-21 05:46:27'),
+(27, 18, 36, '12312312312312312312321', '2022-04-21 06:12:12'),
+(28, 18, 36, '12311111111 12 31231 23 1312 3123 12312 3123 12', '2022-04-21 06:12:21');
+
 -- --------------------------------------------------------
 
 --
@@ -59,6 +83,18 @@ CREATE TABLE `participants` (
   `chat_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `participants`
+--
+
+INSERT INTO `participants` (`id`, `chat_id`, `user_id`) VALUES
+(9, 16, 36),
+(10, 16, 37),
+(11, 17, 36),
+(12, 17, 37),
+(13, 18, 36),
+(14, 18, 37);
 
 -- --------------------------------------------------------
 
@@ -73,6 +109,14 @@ CREATE TABLE `users` (
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `username`, `email`, `password`) VALUES
+(36, 'some name', 'someusername', 'some@gmail.com', '123123'),
+(37, 'some name 2', 'someusername2', 'some2@gmail.com', '321321');
 
 --
 -- Indexes for dumped tables
@@ -110,25 +154,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `chats`
 --
 ALTER TABLE `chats`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `messages`
 --
 ALTER TABLE `messages`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT for table `participants`
 --
 ALTER TABLE `participants`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
