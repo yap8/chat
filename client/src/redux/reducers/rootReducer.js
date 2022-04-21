57cc58043720e12d344c595191d7c1c54ef6110d
yap8 <nebozhenko.arseny@gmail.com>
2022-04-21 03:35:37 +0300

    made all state reset on logout

--- client/src/redux/reducers/rootReducer.js
+++ client/src/redux/reducers/rootReducer.js
@@ -1,12 +1,14 @@
 import { combineReducers } from "redux";
-import chatReducer from "./chatReducer";
-import chatsReducer from "./chatsReducer";
+
 import messagesReducer from "./messagesReducer";
+import { AUTH_LOGOUT } from "../actions/types";
 import requestReducer from "./requestReducer";
+import chatsReducer from "./chatsReducer";
+import chatReducer from "./chatReducer";
 import authReducer from "./authReducer";
 import userReducer from "./userReducer";
 
-const rootReducer = combineReducers({
+const combinedReducer = combineReducers({
   request: requestReducer,
   auth: authReducer,
   user: userReducer,
@@ -14,5 +16,13 @@ const rootReducer = combineReducers({
   chat: chatReducer,
   messages: messagesReducer
 })
+
+const rootReducer = (state, action) => {
+  if (action.type === AUTH_LOGOUT) {
+    state = undefined;
+  }
+
+  return combinedReducer(state, action);
+}
 
 export default rootReducer
