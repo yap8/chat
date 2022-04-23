ed860158ea1ce267915da756de562ffb4757b213
yap8 <nebozhenko.arseny@gmail.com>
2022-04-23 12:37:36 +0300

    reworked Button

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -1,17 +1,29 @@
+import { Link } from 'react-router-dom'
 import mergeClasses from '../helpers/mergeClasses'
 
-const Button = ({ className, children }) => {
+const Button = ({ className, tag, children, ...props }) => {
   const classes = {
-    base: 'bg-blue-500 text-white font-semibold py-2 px-4 rounded transition shadow',
+    base: 'block w-full bg-blue-500 text-white font-semibold py-2 px-4 rounded transition shadow',
     hover: 'hover:bg-blue-700',
     inherited: className
   }
 
-  return (
-    <button
-      className={mergeClasses(classes)}
-    >{children}</button>
-  )
+  switch (tag) {
+    case 'Link':
+      return (
+        <Link
+          className={mergeClasses(classes)}
+          {...props}
+        >{children}</Link>
+      )
+    default:
+      return (
+        <button
+          className={mergeClasses(classes)}
+          {...props}
+        >{children}</button>
+      )
+  }
 }
 
 export default Button
