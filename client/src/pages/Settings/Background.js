7a4da92707d44a630c1de37bc0d77dc2eaa5eb69
yap8 <nebozhenko.arseny@gmail.com>
2022-04-23 13:55:51 +0300

    added Background page

--- client/src/pages/Settings/Background.js
+++ client/src/pages/Settings/Background.js
@@ -1,10 +1,26 @@
 import { Link } from "react-router-dom"
+import PerfectScrollbar from 'react-perfect-scrollbar'
+import 'react-perfect-scrollbar/dist/css/styles.css'
+
+import gradients from "../../assets/data/gradients"
 
 const Background = () => {
   return (
-    <div className="p-4 sm:w-1/2 md:w-1/4 bg-white shadow rounded dark:bg-slate-800">
-      <h1 className="text-xl text-center mb-2 dark:text-white">Background</h1>
-      <Link to="/settings" className="text-blue-500 hover:underline">Go back</Link>
+    <div className="h-4/6 w-1/2 p-2 bg-white shadow rounded dark:bg-slate-800 flex flex-col">
+      <h1 className="text-xl text-center mb-2 p-2 dark:text-white">Backgrounds</h1>
+      <PerfectScrollbar className="flex flex-wrap px-2">
+        {gradients.map(gradient => (
+          <div className="w-1/3 p-2">
+            <button
+              className="w-full"
+            >
+              <h3 className="text-xl text-center dark:text-white mb-2">{gradient.name}</h3>
+              <div className={`${gradient.direction} ${gradient.colors} w-full h-20 rounded`} />
+            </button>
+          </div>
+        ))}
+      </PerfectScrollbar>
+      <Link to="/settings" className="p-2 text-blue-500 hover:underline">Go back</Link>
     </div>
   )
 }
