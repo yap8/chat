f1fe404ab26a590ccfb81c5b6d9a8ace2d8494e4
yap8 <nebozhenko.arseny@gmail.com>
2022-04-27 13:49:53 +0300

    moved content of public to root

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+// slim
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/config/db.php';
+
+$app = AppFactory::create();
+
+// middleware
+require __DIR__ . '/middleware/private.php';
+
+// routes
+require __DIR__ . '/routes/userRoutes.php';
+require __DIR__ . '/routes/homeRoutes.php';
+require __DIR__ . '/routes/chatRoutes.php';
+require __DIR__ . '/routes/messageRoutes.php';
+
+// serve client
+$app->get('/{path:.*}', function($request, $response, $args) {
+  $file = __DIR__ . '/client/build/index.html';
+
+  return $response->write(file_get_contents($file));
+});
+
+$app->run();
